17d4bd1443b605ad7401c79e4d2007cc94ddcc9d
felix <felixmariotto@gmail.com>
2019-06-11 22:00:17 +0800

    moved the actionSprite methods out from the 'contructor'

--- SpriteMixer.js
+++ SpriteMixer.js
@@ -1,6 +1,6 @@
 
 
-// All credit goes to Lee Stemkoski who coded the core texture offseting part :
+// Based on Lee Stemkoski's work who coded the core texture offsetting part :
 // http://stemkoski.github.io/Three.js/Texture-Animation.html
 
 function SpriteMixer() {
@@ -11,11 +11,11 @@ function SpriteMixer() {
 
 
 	function update(delta) { // Update every stored actionSprite if needed.
-		if (this.actionSprites.length > 0) {
+		if (actionSprites.length > 0) {
 			let milliSec = delta * 1000 ; // THREE.Clock.getDelta() returns seconds.
-			for (let i=0 ; i<this.actionSprites.length ; i++) {
-				if ( this.actionSprites[i].paused == false ) {
-					this.updateSprite( this.actionSprites[i], milliSec );
+			for (let i=0 ; i < actionSprites.length ; i++) {
+				if ( actionSprites[i].paused == false ) {
+					updateSprite( actionSprites[i], milliSec );
 				};
 			};
 		};
@@ -45,7 +45,7 @@ function SpriteMixer() {
 				actionSprite.currentTile = 0;
 			}; // Restarts the animation if the last frame was reached at last call.
 
-			this.offsetTexture(actionSprite);
+			offsetTexture(actionSprite);
 
 			if (actionSprite.currentTile == actionSprite.numberOfTiles - 1 &&
 				actionSprite.mustLoop == false &&
@@ -71,9 +71,71 @@ function SpriteMixer() {
 	};
 
 
+
+	// reveal the sprite and play the action only once
+	function playOnce() {
+		this.currentTile = 0 ;
+		this.paused = false ;
+		this.visible = true ;
+		this.mustLoop = false ;
+	};
+
+	// resume the action if it was paused
+	function resume() {
+		this.paused = false ;
+		this.visible = true ;
+	};
+
+	// reveal the sprite and play it in a loop
+	function playLoop() {
+		this.currentTile = 0;
+		this.paused = false ;
+		this.visible = true ;
+		this.mustLoop = true ;
+	};
+
+	// pause the action when it reach the last frame
+	function pauseNextEnd() {
+		this.mustLoop = false;
+	};
+
+	// pause the action on the current frame
+	function pause() {
+		this.paused = true ;
+	};
+
+	// pause and reset the action
+	function stop() {
+		this.currentDisplayTime = 0;
+		this.currentTile = 0;
+		this.paused = true ;
+		if (this.hideWhenFinished == true) {
+			this.visible = false ;
+		};
+		offsetTexture(this);
+	};
+
+
+
+
+
+	/*
+		ActionSprite(textureURL:string, tilesHoriz:integer, tilesVert:integer, numTiles:integer, tileDispDuration:integer)
+			- textureURL : path to the texture.
+			- tilesHoriz : number of frames on the horizontal direction.
+			- tilesVert : number of frames on the vertical direction.
+			- numTiles : total number of frames. As you can see in the exemples,
+			  it does not necessarily equal tilesHoriz*tilesVert, for instance
+			  if the last frames are empty.
+			- tileDispDuration : display duration of ONE FRAME, un milliseconds.
+
+		spriteMixer.ActionSprite() returns a extended THREE.Sprite.
+		All the parameters necessary for the animation are stored inside,
+		but you can still use it as any THREE.Sprite, like scale it etc..
+	*/
 	function ActionSprite(textureURL, tilesHoriz, tilesVert, numTiles, tileDispDuration) {
 
-		let texture = this.loader.load(textureURL);
+		let texture = loader.load(textureURL);
 
 		texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
 		texture.repeat.set( 1/tilesHoriz, 1/tilesVert );
@@ -94,46 +156,16 @@ function SpriteMixer() {
 		sprite.clampWhenFinished = true;
 		sprite.hideWhenFinished = false;
 
-		this.offsetTexture( sprite );
+		offsetTexture( sprite );
 
-		sprite.playOnce = function playOnce() {
-			sprite.currentTile = 0 ;
-			sprite.paused = false ;
-			sprite.visible = true ;
-			sprite.mustLoop = false ;
-		};
+		sprite.playOnce = playOnce;
+		sprite.resume = resume;
+		sprite.playLoop = playLoop;
+		sprite.pauseNextEnd = pauseNextEnd;
+		sprite.pause = pause;
+		sprite.stop = stop;
 
-		sprite.resume = function resume() {
-			sprite.paused = false ;
-			sprite.visible = true ;
-		};
-
-		sprite.playLoop = function playLoop() {
-			sprite.currentTile = 0;
-			sprite.paused = false ;
-			sprite.visible = true ;
-			sprite.mustLoop = true ;
-		};
-
-		sprite.pauseNextEnd = function pauseNextEnd() {
-			sprite.mustLoop = false;
-		};
-
-		sprite.pause = function pause() {
-			sprite.paused = true ;
-		};
-
-		sprite.stop = function stop() {
-			sprite.currentDisplayTime = 0;
-			sprite.currentTile = 0;
-			actionSprite.paused = true ;
-			if (actionSprite.hideWhenFinished == true) {
-				actionSprite.visible = false ;
-			};
-			spriteMixer.offsetTexture(sprite);
-		};
-
-		this.actionSprites.push( sprite );
+		actionSprites.push( sprite );
 		return sprite ;
 	};
 
